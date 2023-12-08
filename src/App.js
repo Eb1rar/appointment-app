caf903a0416e8d4c314baa7800c5e2f0e76248b5
ebrar <ebrkdmr1@gmail.com>
2023-12-08 21:02:01 +0300

    bootstrap install

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>React App</h1>
+    <div className="container">
+      <h1 className="text-danger mx-auto">CLARUS HOSPITAL</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
